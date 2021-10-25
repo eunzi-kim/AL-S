21e11233f95fe6be86db19d3b28b9573f00095ac
rladmswlek <rladmswlek@naver.com>
2021-10-25 23:44:11 +0900

    Add 1025 baekjoon solution

--- 은지/백준/1325_효율적인 해킹.py
+++ 은지/백준/1325_효율적인 해킹.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 N, M = map(int, input().split())
 
@@ -8,33 +9,29 @@ for _ in range(M):
     A, B = map(int, input().split())
     g[B].append(A)
 
-max_n = 0
 ans = set()
+max_n = 0
+
 for i in range(1, N+1):
     visited = [0] * (N+1)
     visited[i] = 1
 
-    stack = [[i, 0]]
-    while stack:
-        x = stack.pop(0)
-        v = x[0]
-        n = x[1]
+    queue = deque()
+    queue.append(i)
+    n = 1
+    while queue:
+        v = queue.popleft()
 
+        for w in g[v]:
+            if visited[w] == 0:
+                queue.append(w)
+                visited[w] = 1
+                n += 1
+        
         if max_n < n:
             max_n = n
             ans = {i}
         elif max_n == n:
             ans.add(i)
 
-        for w in g[v]:
-            if visited[w] == 0:
-                visited[w] = 1
-                stack.append([w, n+1])
-
 print(*sorted(ans))
-
-# result = ""
-# for x in ans:
-#     result += str(x) + " "
-
-# print(result)
