f7f08ef01c9ada57ae7cddd2b45af5c2eb7fe982
wnsdud4197 <wnsdud4197@naver.com>
2021-11-17 00:54:55 +0900

    Add 1116 algorithm solution

---
+++ 준영/백준/06.DP2/9251_LCS.py
@@ -0,0 +1,18 @@
+x = input()
+y = input()
+
+N = len(x)
+M = len(y)
+
+dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        if x[i-1] == y[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[N][M])
+
+
